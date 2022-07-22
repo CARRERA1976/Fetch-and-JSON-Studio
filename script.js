de18944eaff781c36026a47f9d2681d88a365219
CARRERA1976 <108904775+CARRERA1976@users.noreply.github.com>
2022-07-21 21:31:05 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 // TODO: add code here
+      window.addEventListener("load", function() {
+             fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+            return response.json();
+      })
+             .then((json) => {
+             const container = this.document.getElementById("container");
+let astronautHTML = "";
+   for (astronaut of json) {
+    console.log(astronaut);
+    astronautHTML += `
+    <div class="astronaut">
+   <div class="bio">
+      <h3>${astronaut.firstName} ${astronaut.lastName} </h3>
+      <ul>
+         <li>Hours in space:${astronaut ['hoursInSpace']} </li>
+         <li>Active:${astronaut.active}</li>
+         <li>Skills:${astronaut.skills.join(', ')}
+      </ul>
+   </div>
+   <img class="avatar" src=${astronaut.picture}>
+</div> 
+`;
+   }   
+container.innerHTML = astronautHTML;
+  });
+              
+         });
+         
